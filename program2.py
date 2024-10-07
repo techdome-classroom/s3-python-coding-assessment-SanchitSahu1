957fcad651f21534eeb1fd7af6ca5e8574741c37
Sanchit Sahu <163036877+SanchitSahu1@users.noreply.github.com>
2024-10-07 15:51:30 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,7 +4,34 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
+        
+        total = 0
+        
+        for i in range(len(s)):
+           
+            if i + 1 < len(s) and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                total -= roman_to_int[s[i]]
+           
+            else:
+                total += roman_to_int[s[i]]
 
+        return total
 
+# Example usage
+solution = Solution()
+
+# Test cases
+print(solution.romanToInt("III"))      
+print(solution.romanToInt("LVIII"))    
+print(solution.romanToInt("MCMXCIV"))  
